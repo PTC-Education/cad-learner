ad649dd1d392ac2adfd07b0e0bf8e5dc7d5fbe08
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2023-02-08 17:24:51 -0500

    modelling page nav bar css updates

--- questioner/static/questioner/modelling.js
+++ questioner/static/questioner/modelling.js
@@ -1,3 +1,7 @@
-function confirm_alert() {
+function home_confirm_alert() {
     return confirm("Warning, your modeling time will be reset and you will need to restart the challenge. To return to the home page click OK.");
+}
+
+function give_up_confirm_alert() {
+    return confirm("Warning, your modeling time will be reset and you will need to restart the challenge. To directly compare your part to the reference, click continue.");
 }
