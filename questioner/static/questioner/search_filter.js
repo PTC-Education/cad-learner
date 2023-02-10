fd447e7fcb62fb773f4133a475bebbcc04b73e02
Yuanzhe Deng <felixdeng102@gmail.com>
2023-02-10 10:45:36 -0500

    Fix non-reviewer index search and filter

--- questioner/static/questioner/search_filter.js
+++ questioner/static/questioner/search_filter.js
@@ -16,8 +16,9 @@ function search_filter() {
     }
     
     // Get availability if reviewer 
+    var is_reviewer = document.getElementById("is_reviewer").value === "True"; 
     var q_avail; 
-    if (document.getElementById("is_reviewer").value === "True"){
+    if (is_reviewer){
         if (document.getElementById("rev_pub").checked){
             q_avail = "Availability: Published"; 
         } else if (document.getElementById("rev_unpub").checked){
@@ -35,9 +36,19 @@ function search_filter() {
 
     for (var i = 0; i < questions.length; i++) {
         var level = questions[i].getElementsByClassName("difficulty")[0].innerHTML; 
-        var avail = questions[i].getElementsByClassName("availability")[0].innerHTML; 
         var q_name = questions[i].getElementsByClassName("accordion")[0].getElementsByTagName("h3")[0]; 
-        if ((q_type === "all" || q_type === level) && (q_avail === "all" || q_avail === avail) && q_name.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (is_reviewer) {
+            var avail = (
+                q_avail === "all" || 
+                q_avail === questions[i].getElementsByClassName("availability")[0].innerHTML
+            ); 
+        } else {
+            var avail = true; 
+        }
+        if (
+                (q_type === "all" || q_type === level) && avail && 
+                q_name.innerHTML.toUpperCase().indexOf(filter) > -1
+            ) {
             questions[i].style.display = ""; 
         } else {
             questions[i].style.display = "none"; 
