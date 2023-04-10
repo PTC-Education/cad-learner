ee4c228df1359ae6c60ec0784f62b6c59eb2770f
Yuanzhe Deng <felixdeng102@gmail.com>
2023-04-10 10:54:12 -0400

    Add loading spinner to modelling page

--- questioner/static/questioner/loading.js
+++ questioner/static/questioner/loading.js
@@ -1,6 +1,11 @@
-function loading_spin() {
+function loading_index() {
     var lists = document.getElementsByClassName("info_list"); 
     for (var i = 0; i < lists.length; i++) {
         lists[i].innerHTML = "<div class=\"loader\"></div>"; 
     }
+}
+
+function loading_modelling() {
+    const main_content = document.getElementById("main"); 
+    main_content.innerHTML = "<div class=\"loader\"></div>"; 
 }
